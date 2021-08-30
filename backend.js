b40077456eb48ea2673dd30f39259ec5b911ce5f
jonasrdl <jonasriedel@gmx.net>
2021-08-30 21:24:49 +0200

    Added blink function

--- backend.js
+++ backend.js
@@ -13,7 +13,25 @@ app.get('/off', function (req, res) {
   res.sendStatus(200)
 })
 
-app.get('/blink', function (req, res) {})
+app.get('/blink', function (req, res) {
+  const blinkInterval = setInterval(blink, 250)
+
+  function blink() {
+    if (LED.readSync() === 0) {
+      LED.writeSync(1)
+    } else {
+      LED.writeSync(0)
+    }
+  }
+
+  function endBlink() {
+    clearInterval(blinkInterval)
+    LED.writeSync(0)
+    LED.unexport()
+  }
+
+  setTimeout(endBlink, 5000)
+})
 
 app.listen(3000)
 console.log('Server running on port 3000')
